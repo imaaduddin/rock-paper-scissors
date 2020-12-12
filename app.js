75612c9e481bf46561835b5d4e08c6d90f7f510d
Imaad Uddin <imaaduddin@icloud.com>
2020-12-12 14:05:28 -0600

    worked some more with functions

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ const ROCK = "ROCK";
 const PAPER = "PAPER";
 const SCISSORS = "SCISSORS";
 const DEFAULT_USER_CHOICE = ROCK;
+const RESULT_DRAW = "DRAW";
+const RESULT_PLAYER_WINS = "PLAYER WINS";
+const RESULT_COMPUTER_WINS = "COMPUTER WINS";
 
 let gameIsRunning = false;
 
@@ -14,7 +17,28 @@ const getPlayerChoice = function() {
     return DEFAULT_USER_CHOICE;
   }
   return selection;
-}
+};
+
+const getComputerChoice = function() {
+  const randomValue = Math.random();
+  if (randomValue < 0.34) {
+    return ROCK;
+  } else if (randomValue < 0.67) {
+    return PAPER;
+  } else {
+    return SCISSORS;
+  }
+};
+
+const getWinner = function(cChoice, pChoice) {
+  if (cChoice === pChoice) {
+    return RESULT_DRAW;
+  } else if (cChoice === ROCK && pChoice === PAPER || cChoice === PAPER && pChoice === SCISSORS || cChoice === SCISSORS && pChoice === ROCK) {
+    return RESULT_PLAYER_WINS;
+  } else {
+    return RESULT_COMPUTER_WINS;
+  }
+};
 
 
 
@@ -25,5 +49,6 @@ startGameBtn.addEventListener("click", function() {
   gameIsRunning = true;
   console.log("Game is starting....");
   const playerSelection = getPlayerChoice();
-  console.log(playerSelection);
+  const computerChoice = getComputerChoice();
+  // console.log(playerSelection);
 });
